bb7152cb70835d80dc1445ca79a595ece90f7267
hariskurtishi <Hariskay95@gmail.com>
2022-09-26 01:36:44 +0200

    build the homepage

---
+++ pages/product/[slug].js
@@ -0,0 +1,30 @@
+import React from "react";
+import { client, urlFor } from "../../lib/client";
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default ProductDetails;
